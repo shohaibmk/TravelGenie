f98f31f34d03f8bd622a7a96bd9097afbb5ecc54
Shohaib Mallick <shohaibm99@gmail.com>
2024-11-10 21:15:59 -0500

    Create Trip Questions

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 
+
 function Hero() {
     return (
         <div className='flex flex-col items-center justify-center gap-9'>
@@ -10,7 +11,7 @@ function Hero() {
             >
                 <span className='text-[#4834DF]'>Plan your next trip</span> <br />with AI
             </h1>
-            <p className='text-xl text-gray-600 text-center'>Your Personalized Travel Expert, Powered by AI!</p>
+            <p className='text-xl text-gray-600 text-center'>Your Personal Travel Expert, Powered by AI!</p>
             <Link to={'/create-trip'}>
             <Button>Let's get started</Button>
             </Link>
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,10 @@
+import { Input } from "@/components/ui/input"
 import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
 
-    const [place,setPlace]= useState();
+    const [place, setPlace] = useState();
     return (
         <div className='sm:px-10 md:px-32 lg:px-56 xl:px-20 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>
@@ -12,21 +13,27 @@ function CreateTrip() {
             <p className='mt-3 text-gray-600 text-xl'>
                 Provide us basic information about your trip and let the AI plan your trip
             </p>
-            <div className='mt-5'>
+            <div className='mt-20 flex flex-col gap-10'>
                 <div>
                     <h2 className='text-xl my-3 font-medium'>
-                        Where do you want to go?                    </h2>
-                    <GooglePlacesAutocomplete
+                        Where would you like to go?                    </h2>
+                    <GooglePlacesAutocomplete 
                         apiKey={import.meta.env.VITE_KEY_GOOGLE_PLACES_API}
                         selectProps={{
                             place,
-                            onChange:(v)=>{
+                            onChange: (v) => {
                                 setPlace(v);
                                 console.log(v);
                             }
                         }}
                     />
 
+                </div>
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>
+                        How many days would to like to stay there?                    </h2>
+                    <Input placeholder = {'Ex. 3'} type="number"/>
+                    
                 </div>
             </div>
         </div>
