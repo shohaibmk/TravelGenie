c0b23611f18a3ac9b27a5d8d898a1d04cbe03252
Shohaib Mallick <shohaibm99@gmail.com>
2024-11-10 20:10:24 -0500

    Places API

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
     return (
-        <div className='flex flex-col items-center justify-center mx-5 gap-9'>
+        <div className='flex flex-col items-center justify-center gap-9'>
             <h1
                 className='font-extrabold text-[50px] text-center mt-16'
             >
                 <span className='text-[#4834DF]'>Plan your next trip</span> <br />with AI
             </h1>
             <p className='text-xl text-gray-600 text-center'>Your Personalized Travel Expert, Powered by AI!</p>
-            <Button>Get started</Button>
+            <Link to={'/create-trip'}>
+            <Button>Let's get started</Button>
+            </Link>
         </div>
     )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
     return (
-        <div>
-            <h3>Create trip</h3>
+        <div className='sm:px-10 md:px-32 lg:px-56 xl:px-20 px-5 mt-10'>
+            <h2 className='font-bold text-3xl'>
+                Tell us your travel preferences
+            </h2>
+            <p className='mt-3 text-gray-600 text-xl'>
+                Provide us basic information about your trip and let the AI plan your trip
+            </p>
+            <div className='mt-5'>
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>
+                        Where do you want to go?
+                        <GooglePlacesAutocomplete
+                            apiKey = {import.meta.env.VITE_KEY_GOOGLE_PLACES_API}
+                        />
+                    </h2>
+                </div>
+            </div>
         </div>
     )
 }
