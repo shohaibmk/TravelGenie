6926acec8b89b8aac40eb47d5009cdc00ba3e569
Shohaib Mallick <shohaibm99@gmail.com>
2024-11-10 20:17:49 -0500

    Places API

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
+
+    const [place,setPlace]= useState();
     return (
         <div className='sm:px-10 md:px-32 lg:px-56 xl:px-20 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>
@@ -13,11 +15,18 @@ function CreateTrip() {
             <div className='mt-5'>
                 <div>
                     <h2 className='text-xl my-3 font-medium'>
-                        Where do you want to go?
-                        <GooglePlacesAutocomplete
-                            apiKey = {import.meta.env.VITE_KEY_GOOGLE_PLACES_API}
-                        />
-                    </h2>
+                        Where do you want to go?                    </h2>
+                    <GooglePlacesAutocomplete
+                        apiKey={import.meta.env.VITE_KEY_GOOGLE_PLACES_API}
+                        selectProps={{
+                            place,
+                            onChange:(v)=>{
+                                setPlace(v);
+                                console.log(v);
+                            }
+                        }}
+                    />
+
                 </div>
             </div>
         </div>
