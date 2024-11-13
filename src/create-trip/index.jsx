38259e8c82ad71492c5184f749787e66eddbc5a1
Shohaib Mallick <shohaibm99@gmail.com>
2024-11-12 19:48:24 -0500

    Travel Preferences

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -39,19 +39,19 @@ function CreateTrip() {
                             place,
                             onChange: (v) => {
                                 setPlace(v);
-                                // console.log(v);
-                                handleInputChange('location', v)
-                            }
+                                handleInputChange('location', v);
+                            },
                         }}
                     />
+
 
 
                 </div>
                 <div>
                     <h2 className='text-xl my-3 font-medium'>
                         How many days would to like to stay there?</h2>
-                    <Input placeholder={'Ex. 3'} type="number" 
-                        onChange={(e)=>handleInputChange('noOfDays',e.target.value)}
+                    <Input placeholder={'Ex. 3'} type="number" className="focus-visible:ring-1 hover:shadow-md"
+                        onChange={(e) => handleInputChange('noOfDays', e.target.value)}
                     />
                 </div>
                 {/* </div> */}
@@ -59,8 +59,10 @@ function CreateTrip() {
                     <h2 className='text-xl my-3 font-medium'>How much would you like to spend?</h2>
                     <div className="grid grid-cols-3 gap-5">
                         {SelectBudgetOptions.map((item, index) => (
-                            <div key={index} className="p-2 border cursor-pointer rounded-lg hover:shadow-lg"
-                            onClick={()=>handleInputChange('budget',item.title)}
+                            <div key={index} className={`p-2 border cursor-pointer rounded-lg hover:shadow-lg
+                                ${formData?.budget == item.title && 'shadow-xl ring-1 ring-black'}
+                                `}
+                                onClick={() => handleInputChange('budget', item.title)}
                             >
                                 <h2 className="text-4xl">{item.icon}</h2>
                                 <h2 className="font-bold text-lg">{item.title}</h2>
@@ -73,8 +75,10 @@ function CreateTrip() {
                     <h2 className='text-xl my-3 font-medium'>Who do you plan to go with?</h2>
                     <div className="grid grid-cols-3 gap-5">
                         {SelectTravelsList.map((item, index) => (
-                            <div key={index} className="p-2 border cursor-pointer rounded-lg hover:shadow-lg"
-                            onClick={()=>handleInputChange('size',item.people)}
+                            <div key={index} className={`p-2 border cursor-pointer rounded-lg hover:shadow-lg
+                                ${formData?.traveler == item.people && 'shadow-xl ring-1 ring-black'}
+                                `}
+                                onClick={() => handleInputChange('traveler', item.people)}
                             >
                                 <h2 className="text-4xl">{item.icon}</h2>
                                 <h2 className="font-bold text-lg">{item.title}</h2>
